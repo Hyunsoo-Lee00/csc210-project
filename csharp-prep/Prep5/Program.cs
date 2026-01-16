7f825543e106c9959aaf308db720afedf20b4ca7
Hyunsoo Lee <soso01024@gmail.com>
2026-01-15 18:15:23 -0700

    Added a comment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+
+        string name = GetName();
+
+        int num = GetNumber ();
+
+        int year;
+        GetYear(out year);
+
+        int square = Square(num);
+
+        ShowResult(name, square, year);
+    }
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetName()
+    {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void GetYear(out int birthYear)
+    {
+        Console.Write("Enter your birth year: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int Square(int n)
+    {
+        return n * n;
+    }
+
+    static void ShowResult(string name, int square, int year)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+
+        int age = DateTime.Now.Year - year;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
     }
 }
