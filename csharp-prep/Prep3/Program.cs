d781da10444af8c39a8935e30ce9ae52136f8383
Hyunsoo Lee <soso01024@gmail.com>
2026-01-15 17:03:19 -0700

    Added a comment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random RandomN = new Random();
+        int magicnumber = RandomN.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You gueesed it!");
+            }
+        }
     }
 }
