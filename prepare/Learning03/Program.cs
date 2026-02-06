be8a16c29af4914986db0199e0964c86144593ab
Hyunsoo Lee <soso01024@gmail.com>
2026-02-05 22:54:12 -0700

    Added a comment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        f1.SetTop(3);
+        f1.SetBottom(4);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Random rand = new Random();
+        Fraction randomFraction = new Fraction();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = rand.Next(1, 11);
+            int bottom = rand.Next(1, 11);
+            randomFraction.SetTop(top);
+            randomFraction.SetBottom(bottom);
+
+            Console.WriteLine($"Fraction {i}: string: {randomFraction.GetFractionString()} Number: {randomFraction.GetDecimalValue()}");
+        }
     }
 }
